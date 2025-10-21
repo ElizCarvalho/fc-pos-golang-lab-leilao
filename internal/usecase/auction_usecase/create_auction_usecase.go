aa5ac38362624aae94087de2faad490b24aca88f
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-10-21 20:22:57 -0300

    Update auction input validation to include new condition values for product condition

--- internal/usecase/auction_usecase/create_auction_usecase.go
+++ internal/usecase/auction_usecase/create_auction_usecase.go
@@ -14,7 +14,7 @@ type AuctionInputDTO struct {
 	ProductName string           `json:"product_name" binding:"required,min=1"`
 	Category    string           `json:"category" binding:"required,min=2"`
 	Description string           `json:"description" binding:"required,min=10,max=200"`
-	Condition   ProductCondition `json:"condition" binding:"oneof=0 1 2"`
+	Condition   ProductCondition `json:"condition" binding:"oneof=1 2 3"`
 }
 
 type AuctionOutputDTO struct {
